7be5d90cf11f721a1a6a83e450dbc905ea9da161
Adrian <adrianmanso80@gmail.com>
2024-11-19 12:31:32 +0000

    Enhance GraphQL schema and resolvers: add saveBook mutation, update getBooksByAuthor query, and enforce non-nullable fields

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { ApolloServer } from "@apollo/server";
+import { GraphQLError } from "graphql";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import dotenv from "dotenv";
 import { EndPoints } from "./consts/endpoints";
+import { BookType, ErrorData } from "./types/types";
 
 dotenv.config();
 
@@ -12,40 +14,89 @@ const typeDefs = `#graphql
 
     type Book {
         id: Int
-        title: String
+        title: String!
         synopsis: String
-        author: String
-        year: Int
+        author: String!
+        year: Int!
     }
 
     type Query {
         getAllBooks: [Book]
-        getBookByAuthor(author: String): Book
+        getBooksByAuthor(author: String): [Book]
     }
 
+    type Mutation {
+        saveBook(
+            title: String!
+            synopsis: String
+            author: String!
+            year: Int!
+        ): Book
+    }
 `;
 
 const resolvers = {
     Query: {
         getAllBooks: async() => {
-            const response = await fetch(`${API_URL}/${EndPoints.getBooks}`);
-            const data = await response.json();
-            return data;
+            try{
+                const response = await fetch(`${API_URL}/${EndPoints.books}`);
+                const data = await response.json();
+                return data;
+            }catch(err){
+                throw err;
+            }
         },
-        getBookByAuthor: async(_, { author }) => {
-            const response = await fetch(`${API_URL}/${EndPoints.getBooks}`, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    author: author,
-                })
-            });   
-            const data = await response.json();
-            return data;
+        getBooksByAuthor: async(_: any, { author }: {author: string}) => {
+            try{
+                const response = await fetch(`${API_URL}/${EndPoints.search}`, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        author: author,
+                    })
+                });   
+                const data = await response.json();
+                return data;
+            }catch(err){
+                throw err;
+            }
         }
     },
+    
+    Mutation: {
+        saveBook: async(_: any, {title, synopsis, author, year}: BookType) => {
+            try{
+                const response = await fetch(`${API_URL}/${EndPoints.books}`, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        title: title,
+                        synopsis: synopsis,
+                        author: author, 
+                        year: year,
+                    }),
+                });
+
+                if(!response.ok){
+                    // const errorData = await response.json();
+                    throw new GraphQLError("The GraphQL operation is not valid against the server's schema.", {
+                        extensions: {
+                            code: 'GRAPHQL_VALIDATION_FAILED',
+                        },
+                    });
+                };
+                
+                const data = await response.json();
+                return data;
+            }catch(err){
+                throw err;
+            }
+        }
+    }
 };
 
 const server = new ApolloServer({
